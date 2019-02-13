2b0d993ce37fcc350e34fbe8929f64e5a7803569
enrique95ae <enrique95ae@gmail.com>
2019-02-12 18:46:56 -0800

    update

--- HW3_1/HW3_1/HW3_1/MainPage.xaml.cs
+++ HW3_1/HW3_1/HW3_1/MainPage.xaml.cs
@@ -24,8 +24,6 @@ namespace HW3_1
                 pokemonNumber = 151,
                 pokemonName = "Mew",
                 pokemonImg1 = "https://usercontent1.hubstatic.com/14145286_f520.jpg",
-                //pokemonImg2 = "",
-                //pokemonImg3 = "",
                 pokemonDescription = "Mew is a pink, bipedal Pokémon with mammalian features. Its snout is short and wide, and it has triangular ears and large, blue eyes. It has short arms with three-fingered paws, large hind legs and feet with oval markings on the soles, and a long, thin tail ending in an ovoid tip. Its fur is so fine and thin, it can only be seen under a microscope. Mew is said to have the DNA of every single Pokémon contained within its body, and as such is able to learn any attack.",
             };
 
