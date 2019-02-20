0462ca88201af8a262ad46f3f090f2137bb239ba
enrique95ae <enrique95ae@gmail.com>
2019-02-19 18:58:44 -0800

    final projec base

---
+++ FinalProject/FinalProject/FinalProject/NewsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FinalProject
+{
+    public partial class NewsPage : ContentPage
+    {
+        public NewsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
