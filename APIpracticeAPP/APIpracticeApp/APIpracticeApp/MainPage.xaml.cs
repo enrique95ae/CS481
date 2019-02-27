a8e41d75febf6c114845287a214a6e03576c3891
enrique95ae <enrique95ae@gmail.com>
2019-02-26 18:03:19 -0800

    apiapp

---
+++ APIpracticeAPP/APIpracticeApp/APIpracticeApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace APIpracticeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //call here
+        }
+
+        async void GeneratePicButton_Clicked(object sender, EventArgs e)
+        {
+            string dogApiEndpoint = "https://dog.ceo/api/breeds/image/random";
+            Uri dogApiUri = new Uri(dogApiEndpoint);
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetAsync(dogApiUri);
+        }
+    }
+}
---
+++ APIpracticeAPP/APIpracticeApp/APIpracticeApp/Models/Dog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace APIpracticeApp
+{
+    public partial class DogData
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+
+    //public partial class DogData
+    //{
+    //    public static DogData FromJson(string json) => JsonConvert.DeserializeObject<DogData>(json, DogData.Converter.Settings);
+    //}
+
+    //public static class Serialize
+    //{
+    //    public static string ToJson(this DogData self) => JsonConvert.SerializeObject(self, DogData.Converter.Settings);
+    //}
+
+    //internal static class Converter
+    //{
+    //    public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+    //    {
+    //        MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+    //        DateParseHandling = DateParseHandling.None,
+    //        Converters =
+    //        {
+    //            new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+    //        },
+    //    };
+    //}
+}
